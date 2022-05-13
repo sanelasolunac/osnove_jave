a62dc3ff9f4552ae59358110471f42f4d4be81e9
Sanela Solunac <sanelasolunac995@gmail.com>
2022-05-13 20:35:35 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p_13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,11 @@
+package p_13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git clone");
+        System.out.println("git add");
+        System.out.println("git status");
+        System.out.println("git commit -m");
+        System.out.println("git push");
+    }
+}
