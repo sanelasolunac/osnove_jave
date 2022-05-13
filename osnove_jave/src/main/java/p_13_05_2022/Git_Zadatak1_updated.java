049ff604d8e2271b55d8a2bd623f30a61d8c1763
Sanela Solunac <sanelasolunac995@gmail.com>
2022-05-13 21:12:52 +0200

    Update file name

--- osnove_jave/src/main/java/p_13_05_2022/Git_Zadatak1_updated.java
+++ osnove_jave/src/main/java/p_13_05_2022/Git_Zadatak1_updated.java
@@ -1,6 +1,6 @@
 package p_13_05_2022;
 
-public class Git_Zadatak1 {
+public class Git_Zadatak1_updated {
     public static void main(String[] args) {
         System.out.println("git clone");
         System.out.println("git add");
