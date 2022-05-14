4d07a7d990107ac0e51bdff9bb560668ec8bd68c
Sanela Solunac <sanelasolunac995@gmail.com>
2022-05-14 23:04:54 +0200

    Domaci 13.05.2022 Zadatak 3 brisanje iz niza

---
+++ osnove_jave/src/main/java/domaci_zadatak_13_05_2022/Zadatak2.java
@@ -0,0 +1,4 @@
+//package domaci_zadatak_13_05_2022;
+//
+//public class Zadatak2 {
+//}
---
+++ osnove_jave/src/main/java/domaci_zadatak_13_05_2022/Zadatak3.java
@@ -0,0 +1,41 @@
+package domaci_zadatak_13_05_2022;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Zadatak3 {
+    public static void main(String[] args) {
+//        Napisati program koji ima niz brojeva duzine 10. Niz je fiksnih vrednosti (ne u nosi ih korisnik) a zatim
+//        se od korisnika ucitava pozicija elementa koji zeli da izbaci iz niza. Pre i nakon izbacivanja ispisati niz
+//        na ekranu. Ukoliko korisnik unese poziciju van opsega niza, ispisati poruku o gresci.
+//        Primer izvrsenja 1:
+//        Niz je: 1,3,5,6,7,1,3,4,2,9
+//        Unesite poziciju za izbacivanje: 5
+//        Niz nakon brisanja je: 1,3,5,6,7,3,4,2,9
+
+        Scanner s = new Scanner(System.in);
+        ArrayList<Integer> nizBrojeva = new ArrayList<Integer>();
+
+        nizBrojeva.add(1);
+        nizBrojeva.add(2);
+        nizBrojeva.add(3);
+        nizBrojeva.add(4);
+        nizBrojeva.add(5);
+        nizBrojeva.add(6);
+        nizBrojeva.add(7);
+        nizBrojeva.add(8);
+        nizBrojeva.add(9);
+        nizBrojeva.add(3);
+
+        System.out.print("Niz je: " + nizBrojeva);
+        System.out.println();
+
+        System.out.print("Unesite poziciju za izbacivanje: ");
+        int pozicijaZaIzbacivanje = s.nextInt();
+
+        nizBrojeva.remove(pozicijaZaIzbacivanje);
+
+        System.out.println("Niz nakon brisanja je: " + nizBrojeva);
+
+    }
+}
